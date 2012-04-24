6db7587b041d564765ec4baca21d1bf87650ef2f
Matt Durak <mattsnowboard@gmail.com>
2012-04-24 17:01:46 -0400

    Accept challenge is working (creates a new game).

--- src/Wws/Provider/Service/FactoryServiceProvider.php
+++ src/Wws/Provider/Service/FactoryServiceProvider.php
@@ -19,7 +19,7 @@ class FactoryServiceProvider implements ServiceProviderInterface
          * @var closure 
          */
         $app['wws.factory.game'] = $app->share(function($app) {
-            return new \Wws\Factory\GameFactory($app['wws.mapper.dictionary'], $app['wws.mapper.game']);
+            return new \Wws\Factory\GameFactory($app['wws.mapper.dictionary'], $app['wws.mapper.game'], $app['wws.mapper.challenge']);
         });
         
         /**
